520c4a8a68eb10e2608e927ec84c76bd7902dcb5
Sahinur Islam <infosahinur@gmail.com>
2023-10-19 09:36:12 +0600

    half commit wallet page

--- src/Pages/Dashboard/Income/Wallet.jsx
+++ src/Pages/Dashboard/Income/Wallet.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Status from "../../../Components/Common/Status";
+import IncomePercentage from "./IncomePercentage";
 
 const Wallet = () => {
   return (
     <div className="flex flex-col mt-[24px] h-[800px]">
       <Status />
-      <div className="mt-[24px]">wallet</div>
+      <div className="mt-[24px] relative bg-white rounded-[30px] border">
+        <IncomePercentage />
+      </div>
     </div>
   );
 };
