19c68f913efcc4869f7485adc8be1dfb96b07521
Sahinur Islam <infosahinur@gmail.com>
2023-10-24 17:57:42 +0600

    [ADD] setting all page and JoditEditor

--- src/Components/Settings/AboutUs/AboutUs.jsx
+++ src/Components/Settings/AboutUs/AboutUs.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import JoditEditor from "jodit-react";
+import React, { useRef, useState } from "react";
 
 const AboutUs = () => {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className="pt-5">
+      <JoditEditor
+        ref={editor}
+        value={content}
+        onChange={(newContent) => {
+          setContent(newContent);
+        }}
+      />
+      <div className=" cursor-pointer w-full h-12 mt-5 p-2.5 bg-violet-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
+        <div className="text-white text-lg font-semibold font-['Montserrat']">
+          Save
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
