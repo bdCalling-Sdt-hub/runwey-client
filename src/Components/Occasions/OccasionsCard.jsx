47c996eca03275a6dcfe16e752962ccc50c2d7b3
Sahinur Islam <infosahinur@gmail.com>
2023-10-25 11:12:36 +0600

    occasion modal

--- src/Components/Occasions/OccasionsCard.jsx
+++ src/Components/Occasions/OccasionsCard.jsx
@@ -1,20 +1,82 @@
-import React from "react";
+import { Modal } from "antd";
+import React, { useState } from "react";
+import testVideo from "./../../../public/tikvideo.mp4";
+import OccasionsSingleVideo from "./OccasionsSingleVideo";
 
 const OccasionsCard = () => {
+  const [isVideoDelete, setIsVideoDelete] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className="mt-5 rounded-xl ">
-      <div className="relative flex justify-center  items-center mx-auto  ">
-        <img
-          className="rounded-xl h-[150px] w-[200px]  opacity-70"
-          src="https://picsum.photos/200/300"
+    <>
+      <div onClick={showModal} className="cursor-pointer mt-5 rounded-xl ">
+        <div className="relative flex justify-center  items-center mx-auto  ">
+          <img
+            className="rounded-xl h-[150px] w-[200px]  opacity-70"
+            src="https://picsum.photos/200/300"
             // src="https://i.ibb.co/fdL1pfM/5a76c9ebc122b99fe48d3e0e6278ea65.png"
-          alt=""
-        />
-        <p className="text-white absolute text-lg font-medium font-['Montserrat'] z-10">
-          Summer
-        </p>
+            alt=""
+          />
+          <p className="text-white absolute text-lg font-medium font-['Montserrat'] z-10">
+            Summer
+          </p>
+        </div>
       </div>
-    </div>
+      <Modal
+        open={isModalOpen}
+        title={
+          <div className="text-2xl py-2 border-b-[1px] border-primary font-semibold font-['Montserrat'] text-primary">
+            <span>Occasion Details</span>
+          </div>
+        }
+        onCancel={handleCancel}
+        centered
+        footer={[]}
+        width={1000}
+      >
+        <div className="flex flex-col">
+          <div class="mb-4 flex justify-between py-[22px] border-b-primary border-b-[1px]  w-full">
+            <div>
+              <img
+                className="w-[212px] h-[212px]"
+                src="https://i.ibb.co/fdL1pfM/5a76c9ebc122b99fe48d3e0e6278ea65.png"
+                alt=""
+              />
+            </div>
+            <div className="flex gap-3">
+              <div className="w-[120px] cursor-pointer h-9 p-2.5 bg-primary rounded-lg justify-center items-center gap-2.5 inline-flex">
+                <div className="text-white text-sm font-semibold font-['Montserrat']">
+                  Upload
+                </div>
+              </div>
+              <div className="w-[120px] cursor-pointer h-9 p-2.5 bg-white rounded-lg border border-primary justify-center items-center gap-2.5 inline-flex">
+                <div className="text-primary text-sm font-semibold font-['Montserrat']">
+                  Delete
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="border-secondary flex gap-3 border-[1px] rounded-2xl p-[30px]">
+            <OccasionsSingleVideo />
+            <OccasionsSingleVideo />
+            <OccasionsSingleVideo />
+          </div>
+          {/* <div className="cursor-pointer">
+            <div className="w-full h-10 p-2.5 bg-violet-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
+              <div className="text-white text-lg font-semibold font-['Montserrat']">
+                Save
+              </div>
+            </div>
+          </div> */}
+        </div>
+      </Modal>
+    </>
   );
 };
 
