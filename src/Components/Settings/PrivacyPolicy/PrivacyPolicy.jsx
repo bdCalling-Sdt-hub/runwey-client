42f25097f6a4b13fad0705c1dcbe51debd55d528
Sahinur Islam <infosahinur@gmail.com>
2023-12-10 16:12:26 +0600

    [ADD] policy

--- src/Components/Settings/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Components/Settings/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,9 +1,45 @@
 import JoditEditor from "jodit-react";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import baseAxios from "../../../../Config";
 
 const PrivacyPolicy = () => {
   const editor = useRef(null);
   const [content, setContent] = useState("");
+
+  const privacyAndPolicyDataSave = () => {
+    let token = localStorage.getItem("token");
+    baseAxios
+      .post(
+        "api/privacy",
+        { content: content },
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        Swal.fire("Good job!", res.data.message, "success");
+      })
+      .catch((err) => {
+        Swal.fire("Oops!", err.response.data.message, "error");
+      });
+  };
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    baseAxios
+      .get("api/privacy", {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setContent(res.data.data.policy);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="pt-5">
       <JoditEditor
@@ -13,7 +49,10 @@ const PrivacyPolicy = () => {
           setContent(newContent);
         }}
       />
-      <div className=" cursor-pointer w-full h-12 mt-5 p-2.5 bg-violet-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
+      <div
+        onClick={privacyAndPolicyDataSave}
+        className=" cursor-pointer w-full h-12 mt-5 p-2.5 bg-violet-700 rounded-lg justify-center items-center gap-2.5 inline-flex"
+      >
         <div className="text-white text-lg font-semibold font-['Montserrat']">
           Save
         </div>
