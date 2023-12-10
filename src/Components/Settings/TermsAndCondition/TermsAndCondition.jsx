0127e302ca58153319d25f02eabf237dfbf0c37b
Sahinur Islam <infosahinur@gmail.com>
2023-12-10 16:35:45 +0600

    [ADD TermsAndCondition

--- src/Components/Settings/TermsAndCondition/TermsAndCondition.jsx
+++ src/Components/Settings/TermsAndCondition/TermsAndCondition.jsx
@@ -1,9 +1,47 @@
 import JoditEditor from "jodit-react";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Swal from "sweetalert2";
+import baseAxios from "../../../../Config";
 
 const TermsAndCondition = () => {
   const editor = useRef(null);
   const [content, setContent] = useState("");
+
+  const TermsAndConditionDataSave = () => {
+    let token = localStorage.getItem("token");
+    baseAxios
+      .post(
+        "api/termCondition",
+        { termAndCondition: content },
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        Swal.fire("Good job!", res.data.message, "success");
+      })
+      .catch((err) => {
+        Swal.fire("Oops!", err.response.data.message, "error");
+      });
+  };
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    baseAxios
+      .get("api/termCondition", {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setContent(res.data.data.termAndCondition);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="pt-5">
       <JoditEditor
@@ -13,7 +51,10 @@ const TermsAndCondition = () => {
           setContent(newContent);
         }}
       />
-      <div className=" cursor-pointer w-full h-12 mt-5 p-2.5 bg-violet-700 rounded-lg justify-center items-center gap-2.5 inline-flex">
+      <div
+        onClick={TermsAndConditionDataSave}
+        className=" cursor-pointer w-full h-12 mt-5 p-2.5 bg-violet-700 rounded-lg justify-center items-center gap-2.5 inline-flex"
+      >
         <div className="text-white text-lg font-semibold font-['Montserrat']">
           Save
         </div>
